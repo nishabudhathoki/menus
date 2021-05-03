20d16e499987e2387303c37836cac4299eb9091b
nishabudhathoki <69409319+nishabudhathoki@users.noreply.github.com>
2021-05-03 18:40:31 +0545

    Add files via upload

---
+++ menu/menu.js
@@ -0,0 +1,59 @@
+// creating of an items
+const menu=[
+    {
+        id:1,
+        title:"Samay Baji",
+        category:" lunch",
+        price:60,
+        img:"https://trekroute.com/wp-content/uploads/2015/01/samay-Baji-Nepal.jpg",
+        desc:`It is a most delicious food of nepali society.`,
+    },
+
+    {
+        id:2,
+        title:"Dal Bhat",
+        category:" lunch",
+        price:200,
+        img:"https://www.holidify.com/images/cmsuploads/compressed/Dal_Bhat_TarkariNepal_20190527155929.JPG",
+        desc:`It is a traditional and most used food of nepali people.`,
+    },
+
+    {
+        id:3,
+        title:"Chapate",
+        category:"breakfast",
+        price:50,
+        img:"https://www.merokalam.com/wp-content/uploads/2019/12/Chatpate-scaled.jpg",
+        desc:`It is mostly liked by the teengeer boys and girls.`,
+    },
+    {
+        id:4,
+        title:"Momos",
+        category:" breakfast",
+        price:60,
+        img:"https://wild-hearted.com/wp-content/uploads/2019/06/Day1-3-1024x683.jpg",
+         desc:`IT is a most delicious food of nepali society.`,
+    },
+    {
+        id:5,
+        title:"Dedo",
+        category:" lunch",
+        price:60,
+        img:"https://cdn-cms1.hotelrunner.com/assets/photos/original/a17bdef6-2117-44ed-9030-60cb269b58fe.jpeg",
+        desc:`It is a most delicious food used by the himilayan and hilly region of nepal.`,
+    },
+    {
+        id:6,
+        title:"Chapati",
+        category:" breakfast",
+        price:60,
+        img:"https://www.ourfoodandhome.com/wp-content/uploads/2019/01/7.jpg",
+        desc:`IT is a most delicious food  used by the terai region people.`,
+    },
+];
+
+const sectionCenter=document.querySelector(".section-center");
+console.log("sectionCenter");
+window.addEventListener("DOMContentLoaded",function () {
+    console.log("shake and bake");
+});
